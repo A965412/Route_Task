97f9a3f3cdcc7b43113062018b8a5d7fb714ad1e
ahmed <https://github.com/A965412/E-commerce_Application.git>
2024-07-11 14:38:45 +0300

    Organize  Project Structure

---
+++ lib/Widget/Search_Widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Search_Widget extends StatelessWidget {
+  const Search_Widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
