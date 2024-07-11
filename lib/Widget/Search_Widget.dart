1620a2d70b7998e92dc3069474c34290c4ec6be8
ahmed <https://github.com/A965412/E-commerce_Application.git>
2024-07-11 15:16:00 +0300

    FinishSearchWidget

--- lib/Widget/Search_Widget.dart
+++ lib/Widget/Search_Widget.dart
@@ -5,6 +5,33 @@ class Search_Widget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Container(
+      height: 50,
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: const Color.fromARGB(255, 10, 9, 90), width: 1),
+      ),
+      child: const Row(
+        children: [
+          Image(
+            image: AssetImage('assets/images/search_icon.png'),
+            height: 20,
+            width: 20,
+          ),
+          SizedBox(
+            width: 13,
+          ),
+          Text(
+            'What do you search for ?',
+            style: TextStyle(
+                color: Color.fromARGB(255, 147, 146, 146),
+                fontSize: 15,
+                fontWeight: FontWeight.w500),
+          )
+        ],
+      ),
+    );
   }
 }
